3d4859a0ad36c5e916f4136bbac10fc2afd13639
Ubuntu <ubuntu@ip-172-31-25-18.us-west-2.compute.internal>
2013-07-10 22:21:39 +0000

    More

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   //response.send('Hello World 2!');
-  var index_buff = fs.readFile("index.html");
+  var index_buff = new Buffer(30);
+  index_buff.write(fs.readFileSync("index.html"));
   //response.send(index_buff.toString())
   response.send(index_buff.toString());
   //response.send('Ania');
