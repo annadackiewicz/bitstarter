16f3ee8533123cc69c85d00cbafd967b475849b5
Ubuntu <ubuntu@ip-172-31-25-18.us-west-2.compute.internal>
2013-07-10 21:55:42 +0000

    Changed filename to string, second try

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   //response.send('Hello World 2!');
-  var buff_index = fs.readFile(index.html)
+  var buff_index = fs.readFile("index.html")
   response.send(buff_index.toString())
 });
 
